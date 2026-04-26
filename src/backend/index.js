093ea4f1fe2bf68251c8242cd4ca7ec7d05ac507
Julian <jdperez0828@gmail.com>
2026-04-26 16:27:31 -0400

    Database integration and dorm + review GETs with error handling

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,25 +1,46 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+const prisma = require('./db');
 
-const dorms = [
-    { id: 1, name: 'Hume Hall' },
-    { id: 2, name: 'Beaty Towers' },
-    { id: 3, name: 'Jennings Hall' },
-];
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ message: 'GatorHall API is running!' });
 });
 
-app.get('/dorms', (req, res) => {
+app.get('/dorms', async (req, res) => {
+    const dorms = await prisma.dorm.findMany();
     res.json(dorms);
 });
 
-app.get('/dorms/:id/reviews', (req, res) => {
-    const dormID = req.params.id;
-    res.json({ dormID, reviews: [] });
+app.get('/dorms/:id/reviews', async (req, res) => {
+    try {
+        // Fetch the requested Dorm by ID
+        const dormID = req.params.id;
+
+        // Ensure the requested dorm is real.
+        const dormFound = await prisma.dorm.findUnique({
+            where: { id: parseInt(dormID) },
+        });
+
+        // Return 404 if dorm requested is not in database
+        if (!dormFound) {
+            return res.status(404).json({ error: 'Dorm not found.' });
+        }
+        // Fetch reviews for the requested dorm
+        const reviews = await prisma.review.findMany({
+            where: { dormId: parseInt(dormID) },
+        });
+
+        // Return the reviews for dorm
+        res.json(reviews);
+    }
+    catch (error) {
+        // Return 500 error code
+        res.status(500).json({ error: 'Something went wrong.' });
+    }
 });
 
 app.listen(3000, () => {
