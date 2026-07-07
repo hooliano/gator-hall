37fb6c731e8b6fc12a7067c5bfccb229fea827ed
hooliano <jdperez0828@gmail.com>
2026-07-07 18:45:43 -0400

    Fix CORS web URLs

--- src/backend/index.js
+++ src/backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'http://gatorhall.netlify.app'
+        'https://gatorhall.netlify.app'
     ],
     credentials: true
 }));
